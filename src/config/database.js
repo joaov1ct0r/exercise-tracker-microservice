b4ea4d687cf2008f8d29898e2199d8d8d711e35c
Joao Victor <jov9051@outlook.com>
2022-04-06 18:46:31 -0300

    feat: dbConnection criado em database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,15 @@
 import mongoose from 'mongoose';
+
+const dbConnection = () => {
+    mongoose.connect(
+        process.env.MONGO_URI,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        },
+        error => {
+            if (error) throw error;
+            else console.log('Conectado a DB!');
+        }
+    );
+};
