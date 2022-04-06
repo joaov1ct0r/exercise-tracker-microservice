8baefe98ad228839354248594fe35b8b5205d275
Joao Victor <jov9051@outlook.com>
2022-04-06 19:08:36 -0300

    feat: handleNewUser criado em userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,27 @@
 import Exercises from '../models/exercisesModel';
 
 import User from '../models/userModel';
+
+const handleNewUser = (req, res) => {
+    let username = req.body.username;
+    let _id = '';
+
+    User.findOne({ username: username }, (err, data) => {
+        if (!err && data === null) {
+            let newUser = new User({
+                username: username
+            });
+
+            newUser.save((error, data) => {
+                if (error) throw error;
+                else {
+                    _id = data['_id'];
+
+                    return res.json({ username: username, _id: _id });
+                }
+            });
+        } else {
+            return res.json({ error: 'Usuario ja cadastrado!' });
+        }
+    });
+};
