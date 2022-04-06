789e60ed6c5ba4a45d627973f7bcaabae01c481c
Joao Victor <jov9051@outlook.com>
2022-04-06 18:59:13 -0300

    feat: exercisesSchema criado em exercisesModel.js

--- src/models/exercisesModel.js
+++ src/models/exercisesModel.js
@@ -1,3 +1,10 @@
 import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
+
+const exercisesSchema = new Schema({
+    userId: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, min: 1, required: true },
+    date: { type: Date, default: Date.now }
+});
