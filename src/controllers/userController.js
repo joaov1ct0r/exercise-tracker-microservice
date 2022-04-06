20254a33a3f75f859aa4998aea3189548d8164aa
Joao Victor <jov9051@outlook.com>
2022-04-06 19:19:30 -0300

    feat: handleNewExercise criado em userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -26,4 +26,53 @@ const handleNewUser = (req, res) => {
     });
 };
 
+const handleNewExercise = (req, res) => {
+    if (req.params._id === undefined) {
+        return res.json({ error: '_id não encontrado!' });
+    }
+
+    if (req.body.description === undefined) {
+        return res.json({ error: 'description não encontrado!' });
+    }
+
+    if (req.body.duration === undefined) {
+        return res.json({ error: 'duration não encontrado!' });
+    }
+
+    let userId = req.params._id;
+
+    let description = req.body.description;
+
+    let duration = req.body.duration;
+
+    let date =
+        req.body.date !== undefined ? new Date(req.body.date) : new Date();
+
+    User.findById(userId, (err, data) => {
+        if (!err && data !== null) {
+            let newExercise = new Exercises({
+                userId: userId,
+                description: description,
+                duration: duration,
+                date: date
+            });
+
+            newExercise.save((error, data2) => {
+                if (error) throw error;
+                else {
+                    return res.json({
+                        username: data['username'],
+                        _id: data['_id'],
+                        description: data2['description'],
+                        duration: Number(data2['duration']),
+                        date: data2['date'].toDateString()
+                    });
+                }
+            });
+        } else {
+            return res.json({ error: 'Usuario não encontrado' });
+        }
+    });
+};
+
 export { handleNewUser };
