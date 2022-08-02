ea081db6ab8ecc3a44beec8cbf1d5a80bfb48f00
Joao Victor <jov9051@outlook.com>
2022-08-02 12:35:04 -0300

    build: @types/express instalado

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+import cors from "cors";
+
+import dbConnection from "./config/database.js";
+
+import userRouter from "./routes/userRoutes.js";
+
+const app = express();
+
+dbConnection();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use("/api", userRouter);
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log("Server running");
+});
